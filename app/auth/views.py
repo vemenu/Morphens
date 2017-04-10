12b264ab1ef02da684ad84e209733a8d01c9182b
Joshua <100age@163.com>
2017-04-10 18:17:53 +0800

    feat:chapter8

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,7 +11,7 @@ def login():
     form = Login_Form()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_passwd(form.password.data):
+        if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Ivalied username or password')
