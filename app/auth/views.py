09f0f2ace59fe72acabdd9d9c2524cc356a2b219
Joshua <100age@163.com>
2017-04-12 17:55:11 +0800

    feat(model):add password_hash

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,7 +27,6 @@ def logout():
     flash('You have been logged out')
     return redirect(url_for('main.index'))
 
-
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -40,17 +39,42 @@ def register():
         token = user.generate_confirmation_token()
         send_mail(user.email, 'Confirm You Account', 'auth/email/confirm', user=user, token=token)
         flash('A confirmation email has been sent to you by email.')
-        return redirect(url_for('main.index'))
+        return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
-
 
 @auth.route('/confirm/<token>')
 @login_required
 def confirm(token):
     if current_user.confirmed:
-        return redirect(url_for('main/index'))
+        return redirect(url_for('main.index'))
     if current_user.confirm(token):
         flash('You have confirmed you accout.Thanks!')
     else:
         flash('The confirmation links is invalid or has expired!')
+    return redirect(url_for('main.index'))
+
+
+@auth.before_app_request
+def befor_requst():
+    if current_user.is_authenticated:
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.endpoint[:5] != 'auth.' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_mail(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has benn sent to you by email')
     return redirect(url_for('main.index'))
